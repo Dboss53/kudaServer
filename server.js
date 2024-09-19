827bb607dae74235dff16f73ec700e0fd9d5038f
Dboss53 <Dboss53787@gmail.com>
2024-09-19 22:42:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 // emails credentials
 const userEmail = "Youokay801@gmail.com";
 const pass = "cnwupnrjfguifecl";
-
+// ibom 2 oct
 // Middleware
 app.use(express.json());
 
